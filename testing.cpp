013e46364768cfd0b20da38da9665bd72f269766
jroush <jroush.tesmods@gmail.com>
2011-01-03 14:41:42 -0500

    Added testing cpp file
    This file is strictly for debugging purposes, but since it is listed
    in COEF.vcproj, the project will not compile unless it is included with
    the rest of the code.

---
+++ testing.cpp
@@ -0,0 +1,54 @@
+// Redefing macros to export class definitions
+#undef IMPORT
+#define IMPORT
+#undef IMPORTCLASS
+#define IMPORTCLASS __declspec(dllexport)
+
+// Game vs CS
+#define OBLIVION    
+//#undef OBLIVION
+
+#pragma warning (disable: 4251) // template interface needs dll instance blah blah blah
+
+//// included classes
+//#include "API/Actors/ActorValues.h"
+//#include "API/BSTypes/BSSimpleList.h"
+//#include "API/BSTypes/BSStringT.h"
+//#include "API/CSDialogs/TESStringInputDialog.h"
+//#include "API/CSDialogs/TESDialog.h"
+//#include "API/CSDialogs/DialogExtraData.h"
+//#include "API/ExtraData/BSExtraData.h"
+//#include "API/ExtraData/ExtraDataList.h"
+//#include "API/GameWorld/TESObjectCELL.h"
+//#include "API/Magic/Magic.h"
+//#include "API/Magic/EffectSetting.h"
+//#include "API/Magic/EffectItem.h"
+//#include "API/Magic/MagicTarget.h"
+//#include "API/Magic/MagicCaster.h"
+//#include "API/Magic/MagicItem.h"
+//#include "API/Magic/MagicItemForm.h"
+//#include "API/Magic/MagicItemObject.h"
+//#include "API/Magic/ActiveEffect.h"
+//#include "API/Magic/ActiveEffects.h"
+//#include "API/Magic/ValueModifierEffects.h"
+//#include "API/Magic/AssociatedItemEffects.h"
+//#include "API/NiTypes/NiTArray.h"
+//#include "API/NiTypes/NiTList.h"
+//#include "API/NiTypes/NiTMap.h"
+//#include "API/Settings/Settings.h"
+//#include "API/TES/DynamicCast.h"
+//#include "API/TES/Mechanics.h"
+//#include "API/TES/MemoryHeap.h"
+//#include "API/TES/TESDataHandler.h"
+//#include "API/TESFiles/TESFile.h"
+//#include "API/TESForms/BaseFormComponent.h"
+//#include "API/TESForms/TESForm.h"
+//#include "API/TESForms/TESObject.h"
+//#include "API/TESForms/TESObjectREFR.h"
+
+// testfunc
+void testfunc()
+{
+}
+
+
