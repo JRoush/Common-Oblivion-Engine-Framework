dd49a94a0e2e95bd7f473677e738bc7c44c64b30
jroush <jroush.tesmods@gmail.com>
2011-01-25 23:38:28 -0500

    Added TESFileFormats::GetFormFromCode()
    This provides a centralized way of retrieving a TESForm* for any of the
    various codes used in serialization

--- Components/TESFileFormats.cpp
+++ Components/TESFileFormats.cpp
@@ -1,30 +1,49 @@
 #include "Components/TESFileFormats.h"
 #include "API/TESFiles/TESFile.h"
+#include "API/TESForms/TESForm.h"
+#include "API/Magic/EffectSetting.h"
 #include "API/Actors/ActorValues.h"
 
 bool TESFileFormats::ResolveModValue(UInt32& value, TESFile& file, UInt32 resolutionType)
 {
     switch (resolutionType)
     {
-    case kResolutionType_None:
+    case kResType_None:
         return true;
-    case kResolutionType_FormID:
-        return TESForm::ResolveFormID(value,file);
-    case kResolutionType_MgefCode:
+    case kResType_FormID:
+        if (value == 0) return true;
+        else return TESForm::ResolveFormID(value,file);
+    case kResType_MgefCode:
     {
         // returns invalid code on failure      
         if (value < 0x80000000) return true;   // code is not dynamic
-        UInt32 fmid = (value << 0x18) | 0xFFFFFF;    // create a dummy formid
+        UInt32 fmid = value << 0x18;    // create a dummy formid
         if (!TESForm::ResolveFormID(fmid,file))  return false; // failed to resolve
         value = (value & 0xFFFFFF00) | (fmid >> 0x18);
         return true;
     }
-    case kResolutionType_ActorValue:
+    case kResType_ActorValue:
     {
-        if (TESForm::IsFormIDBuiltin(value) || value == ActorValues::kActorVal__UBOUND) return true; 
+        if (value == 0 || TESForm::IsFormIDBuiltin(value) || value == ActorValues::kActorVal__UBOUND) return true; 
         else return TESForm::ResolveFormID(value,file);
     }
     default:
         return false;
     }
 }
+TESForm* TESFileFormats::GetFormFromCode(UInt32 code, UInt32 resolutionType)
+{
+    switch (resolutionType)
+    {
+    case kResType_FormID:
+        return TESForm::LookupByFormID(code);
+    case kResType_MgefCode:
+        return EffectSettingCollection::LookupByCode(code);
+    case kResType_ActorValue:
+        // TODO - connect to ExtendedActorValues component once it is written
+        if (TESForm::IsFormIDBuiltin(code) || code == ActorValues::kActorVal__UBOUND) return 0; 
+        else return TESForm::LookupByFormID(code);
+    default:
+        return 0;
+    }
+}   
--- Components/TESFileFormats.h
+++ Components/TESFileFormats.h
@@ -10,6 +10,7 @@
 
 // argument classes
 class   TESFile;    // API/TESFiles/TESFile.h
+class   TESForm;    // API/TESForms/TESForm.h
 
 class COMPONENT_EXPORT TESFileFormats
 {
@@ -17,13 +18,14 @@ public:
 
     enum ResolutionTypes
     {
-        kResolutionType_None          = 0x00,   // for all values that do not depend on load order
-        kResolutionType_FormID        = 0x01,   // iif >= 0x800, highest byte is index of mod in load order
-        kResolutionType_MgefCode      = 0x02,   // [OBME specific] iif >= 0x80000000, lowest byte is index in load order
-        kResolutionType_ActorValue    = 0x03,   // same as formIDs, but the value 0xFFFFFFFF is not resolved
+        kResType_None          = 0x00,   // for all values that do not depend on load order
+        kResType_FormID        = 0x01,   // iif >= 0x800, highest byte is index of mod in load order
+        kResType_MgefCode      = 0x02,   // [OBME specific] iif >= 0x80000000, lowest byte is index in load order
+        kResType_ActorValue    = 0x03,   // same as formIDs, but the value 0xFFFFFFFF is not resolved
     };
 
     // methods
-    static bool     ResolveModValue(UInt32& value, TESFile& file, UInt32 resolutionType); // resolve value from mod file
+    static bool         ResolveModValue(UInt32& value, TESFile& file, UInt32 resolutionType); // resolve value from mod file
+    static TESForm*     GetFormFromCode(UInt32 code, UInt32 resolutionType); // returns a TESForm* if possible for the specified code
 
 };
